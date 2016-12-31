d99b2f6058b7b630fca3068a5a337569dbe6302f
Mark Mandel <markmandel@google.com>
2016-12-31 01:07:30 +0000

    Basic movement on the paddle.

---
+++ Assets/Client/PlayerInput.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+// Moves the Player (paddle and camera) around
+// when using the keyboard
+public class PlayerInput : MonoBehaviour
+{
+    // speed of movement, in all directions
+    [SerializeField] private float speed = 0.1f;
+
+    void FixedUpdate()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+
+        transform.Translate(deltaX, 0, deltaZ);
+    }
+}
