018b667c15458a8911f779bd8e32271221ae5f76
Mark Mandel <markmandel@google.com>
2017-01-02 14:25:29 -0800

    Initial test written. That's something.

---
+++ Assets/Client/Common/TriggerEventObservableTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Assets.Client.Common
+{
+    [TestFixture]
+    public class TriggerEventObservableTest
+    {
+        [Test]
+        public void TriggerEnter()
+        {
+             Assert.True(true);
+        }
+    }
+}
