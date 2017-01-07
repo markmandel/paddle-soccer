99de014925746d553362407e50a575294574a892
Mark Mandel <markmandel@google.com>
2017-01-07 11:42:34 -0800

    Minor tweaks and refactorings

--- Assets/Client/Controllers/ScoreController.cs
+++ Assets/Client/Controllers/ScoreController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 namespace Client.Controllers
 {
+    // Manages the score for both players
     public class ScoreController : MonoBehaviour
     {
         private static readonly string PlayerOneGoal = "/Soccerfield/PlayerGoal.1";
@@ -13,22 +14,26 @@ namespace Client.Controllers
         private int playerOneScore;
         private int playerTwoScore;
 
+        // --- Messages ---
+
         private void Start()
         {
             playerOneScore = 0;
             playerTwoScore = 0;
 
-            var p1Observable = GameObject.Find(PlayerOneGoal).GetComponent<TriggerObservable>();
-            p1Observable.TriggerEnter += OnBallGoal(_ => playerOneScore += 1);
-            p1Observable.TriggerEnter += OnBallGoal(OnGoal);
+            var p1Goal = GameObject.Find(PlayerOneGoal).GetComponent<TriggerObservable>();
+            p1Goal.TriggerEnter += OnBallGoal(_ => playerOneScore += 1);
+            p1Goal.TriggerEnter += OnBallGoal(OnGoal);
 
-            var p2Observable = GameObject.Find(PlayerTwoGoal).GetComponent<TriggerObservable>();
-            p2Observable.TriggerEnter += OnBallGoal(_ => playerOneScore += 1);
-            p2Observable.TriggerEnter += OnBallGoal(OnGoal);
+            var p2Goal = GameObject.Find(PlayerTwoGoal).GetComponent<TriggerObservable>();
+            p2Goal.TriggerEnter += OnBallGoal(_ => playerOneScore += 1);
+            p2Goal.TriggerEnter += OnBallGoal(OnGoal);
         }
 
+        // --- Functions --
+
         // returns a event handler for the TriggerObservable that
-        //only fires when the ball goes into the goal.
+        // only fires when the ball goes into the goal.
         private static TriggerObservable.Triggered OnBallGoal(Action<Collider> action)
         {
             return collider =>
--- TODO.md
+++ TODO.md
@@ -2,6 +2,7 @@
 
 - Reset the ball a few seconds after there is a goal
 - Show "GOAL" when a goal happens
+- Make it so a player can't go into the goal area
 - Track scores once the go through the goal area (on screen)
 - Put a red and blue highlight on each goal, so you can tell which is which
 - Start networking
