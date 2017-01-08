20dd748bf2f64162902ffe97c9a438a98bd2d2b2
Mark Mandel <markmandel@google.com>
2017-01-07 18:22:38 -0800

    Some more refactoring that pulls common functionsinto Goal Class.

--- Assets/Client/Controllers/ScoreController.cs
+++ Assets/Client/Controllers/ScoreController.cs
@@ -8,9 +8,6 @@ namespace Client.Controllers
     // Manages the score for both players
     public class ScoreController : MonoBehaviour
     {
-        public static readonly string PlayerOneGoal = "/Soccerfield/PlayerGoal.1";
-        public static readonly string PlayerTwoGoal = "/Soccerfield/PlayerGoal.2";
-
         private int playerOneScore;
         private int playerTwoScore;
 
@@ -21,29 +18,16 @@ namespace Client.Controllers
             playerOneScore = 0;
             playerTwoScore = 0;
 
-            var p1Goal = GameObject.Find(PlayerOneGoal).GetComponent<TriggerObservable>();
-            p1Goal.TriggerEnter += OnBallGoal(_ => playerOneScore += 1);
-            p1Goal.TriggerEnter += OnBallGoal(OnGoal);
+            var p1Goal = Goals.FindPlayerOneGoal().GetComponent<TriggerObservable>();
+            p1Goal.TriggerEnter += Goals.OnBallGoal(_ => playerOneScore += 1);
+            p1Goal.TriggerEnter += Goals.OnBallGoal(OnGoal);
 
-            var p2Goal = GameObject.Find(PlayerTwoGoal).GetComponent<TriggerObservable>();
-            p2Goal.TriggerEnter += OnBallGoal(_ => playerOneScore += 1);
-            p2Goal.TriggerEnter += OnBallGoal(OnGoal);
+            var p2Goal = Goals.FindPlayerTwoGoal().GetComponent<TriggerObservable>();
+            p2Goal.TriggerEnter += Goals.OnBallGoal(_ => playerOneScore += 1);
+            p2Goal.TriggerEnter += Goals.OnBallGoal(OnGoal);
         }
 
         // --- Functions --
-
-        // returns a event handler for the TriggerObservable that
-        // only fires when the ball goes into the goal.
-        private static TriggerObservable.Triggered OnBallGoal(Action<Collider> action)
-        {
-            return collider =>
-            {
-                if(collider.name == Ball.Name)
-                {
-                    action(collider);
-                }
-            };
-        }
 
         private void OnGoal(Collider _)
         {
---
+++ Assets/Client/Game/Goals.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Client.Common;
+using UnityEngine;
+
+namespace Client.Game
+{
+    public class Goals
+    {
+        public static readonly string PlayerOneGoal = "/Soccerfield/PlayerGoal.1";
+        public static readonly string PlayerTwoGoal = "/Soccerfield/PlayerGoal.2";
+
+        public static GameObject FindPlayerTwoGoal()
+        {
+            return GameObject.Find(Goals.PlayerTwoGoal);
+        }
+
+        public static GameObject FindPlayerOneGoal()
+        {
+            return GameObject.Find(Goals.PlayerOneGoal);
+        }
+
+        // returns a event handler for the TriggerObservable that
+        // only fires when the ball goes into the goal.
+        public static TriggerObservable.Triggered OnBallGoal(Action<Collider> action)
+        {
+            return collider =>
+            {
+                if(collider.name == Ball.Name)
+                {
+                    action(collider);
+                }
+            };
+        }
+    }
+}
